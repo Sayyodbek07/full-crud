76135cbc0eef7559a9a5e4984eb77eb17b9fd145
Sayyodbek <sarvinoztogayeva603@gmail.com>
2025-11-20 18:03:06 +0500

    finish the full crud

--- myproject/settings.py
+++ myproject/settings.py
@@ -78,8 +78,8 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.pgAdmin 4',
+        'NAME': BASE_DIR / 'pgAdmin 4',
     }
 }
 
